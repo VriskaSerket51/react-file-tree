45a36a90b9d4e2d9e9020ad070a8ec1727073465
vriskaserket51 <serketvriska51@gmail.com>
2024-09-12 22:51:59 +0900

    Initial Commit

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -1,91 +1,91 @@
-'use client';
+"use client";
 
 import {
   createFileTree,
   Directory,
   ToggleFileTree,
-} from 'react-toggle-file-tree';
+} from "@ireves/react-file-tree";
 
 export default function Home() {
   const handleFileClick = () => {
-    console.log('handleFileClick');
+    console.log("handleFileClick");
   };
 
   const handleDirectoryClick = () => {
-    console.log('handleDirectoryClick');
+    console.log("handleDirectoryClick");
   };
   const list = [
     {
-      localPath: '/',
-      fileName: 'inch.txt',
+      localPath: "/",
+      fileName: "inch.txt",
       properties: {
-        size: '1 bit',
-        anything: 'possible',
-        a: 'b',
+        size: "1 bit",
+        anything: "possible",
+        a: "b",
       },
     },
     {
-      localPath: '/fruit',
-      fileName: 'apple.docs',
+      localPath: "/fruit",
+      fileName: "apple.docs",
       properties: {
-        sizezz: '12mb',
-        azvv: 'asdfasd',
+        sizezz: "12mb",
+        azvv: "asdfasd",
       },
     },
     {
-      localPath: '/fruit',
-      fileName: 'banana.png',
+      localPath: "/fruit",
+      fileName: "banana.png",
       properties: {
-        sizezz: '12mb',
-        azvv: 'asdfasd',
+        sizezz: "12mb",
+        azvv: "asdfasd",
       },
     },
     {
-      localPath: '/vehicle/car',
-      fileName: 'truck.pdf',
+      localPath: "/vehicle/car",
+      fileName: "truck.pdf",
       properties: {
-        sizezz: '12mb',
-        azvv: 'asdfasd',
+        sizezz: "12mb",
+        azvv: "asdfasd",
       },
     },
     {
-      localPath: '/vehicle/car',
-      fileName: 'taxi.ppt',
+      localPath: "/vehicle/car",
+      fileName: "taxi.ppt",
       properties: {
-        sizezz: '12mb',
-        azvv: 'asdfasd',
+        sizezz: "12mb",
+        azvv: "asdfasd",
       },
     },
     {
-      localPath: '/plant/tree',
-      fileName: 'oak.json',
+      localPath: "/plant/tree",
+      fileName: "oak.json",
       properties: {
-        sizezz: '10mb',
-        azvv: 'asdfasd',
+        sizezz: "10mb",
+        azvv: "asdfasd",
       },
     },
     {
-      localPath: '/plant/tree',
-      fileName: 'maple.zip',
+      localPath: "/plant/tree",
+      fileName: "maple.zip",
       properties: {
-        sizezz: '8mb',
-        azvv: 'asdfasd',
+        sizezz: "8mb",
+        azvv: "asdfasd",
       },
     },
     {
-      localPath: '/plant/flower',
-      fileName: 'cherry-blossom.numbers',
+      localPath: "/plant/flower",
+      fileName: "cherry-blossom.numbers",
       properties: {
-        sizezz: '5mb',
-        azvv: 'asdfasd',
+        sizezz: "5mb",
+        azvv: "asdfasd",
       },
     },
     {
-      localPath: '/plant/fruit',
-      fileName: 'apple-tree.csv',
+      localPath: "/plant/fruit",
+      fileName: "apple-tree.csv",
       properties: {
-        sizezz: '15mb',
-        azvv: 'asdfasd',
+        sizezz: "15mb",
+        azvv: "asdfasd",
       },
     },
   ];
